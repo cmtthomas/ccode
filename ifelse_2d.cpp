7e0d0083d5419861e96abfb1e7ff452d1ed9a216
Caleb Thomas <=>
2019-04-18 13:33:46 -0700

    AA

---
+++ ifelse_2d.cpp
@@ -0,0 +1,48 @@
+// ifelse_2d.cpp
+//compile g++ ifelse_2d.cpp
+
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+int main()
+{
+		int r,c;//row and column
+		string word = "word";
+		int val = 0;
+		int m[8][8] = {
+			{1,4,4,4,4,4,4,1} ,
+			{2,3,3,3,3,3,3,2} ,
+			{2,3,3,3,3,3,3,2} ,
+			{2,3,3,3,3,3,3,2} ,
+			{2,3,3,3,3,3,3,2} ,
+			{2,3,3,3,3,3,3,2} ,
+			{2,3,3,3,3,3,3,2},
+			{1,4,4,4,4,4,4,1}
+		};
+
+		  for (r = 0; r < 8; r++){
+				for (c = 0; c < 8; c++){
+				  val = m[r][c];
+				  //switch
+					  switch (val){
+							case 1:
+								word = "*";
+								break;
+							case 2:
+							  word = "|";
+							  break;
+							case 3:
+							  word = "/";
+								break;
+							case 4:
+							  word = "-";
+								break;
+						}
+					//end switch
+				  }
+				}//end c
+			cout<<"\n";
+		}//end r
+	return 0;
+}
